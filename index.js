bf2cce332ae8f08ed5d69588fbacd9ed797f1d49
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-13 22:35:36 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const hq=42;
+function distanceFromHqInBlocks(pickup) {
+    
+    return Math.abs(pickup - hq);
+    
+}
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks (pickup) * 264;
+}
+function distanceTravelledInFeet(pickupBlock, destinationBlock) {
+    return Math.abs(pickupBlock - destinationBlock) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare =0;
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance > 400) {
+        if (distance > 2000) {
+            if (distance > 2500) {
+                fare = `cannot travel that far`;
+            } else {
+                fare = 25;
+            }
+        } else {
+            fare = (distance - 400) * .02;
+        }
+    } else {
+        fare = 0;
+    }
+    return fare;
+}
